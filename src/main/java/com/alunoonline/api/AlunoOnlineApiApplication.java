8b825e677b3b62ded299451032c954ed77f3a423
jersonb <jersonlbrito@hotmail.com>
2024-11-24 16:52:59 -0300

    feat: fix open browser on start to any SOs

--- src/main/java/com/alunoonline/api/AlunoOnlineApiApplication.java
+++ src/main/java/com/alunoonline/api/AlunoOnlineApiApplication.java
@@ -3,17 +3,18 @@ package com.alunoonline.api;
 import com.alunoonline.api.configs.SwaggerConfiguration;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "API Aluno Online", version = "1", description = "API do Jersinho"))
 public class AlunoOnlineApiApplication {
-
     public static void main(String[] args) {
         var application = new SpringApplication(AlunoOnlineApiApplication.class);
         application.addListeners(new SwaggerConfiguration());
         SpringApplication.run(AlunoOnlineApiApplication.class, args);
+
     }
 
 }
--- src/main/java/com/alunoonline/api/configs/BrowserLauncher.java
+++ src/main/java/com/alunoonline/api/configs/BrowserLauncher.java
@@ -1,36 +1,57 @@
 package com.alunoonline.api.configs;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import java.awt.*;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
 @Component
-
+@Slf4j
 public class BrowserLauncher{
+
+    @Value("${server.port:8080}")
+    private int port;
 
     @EventListener(ApplicationReadyEvent.class)
 
-    public void launchBrowser() throws IOException, URISyntaxException {
+    public void startBrowser() {
 
-        System.setProperty("java.awt.headless", "false");
+        String url = "http://localhost:" + port;
+        try {
+            if (Desktop.isDesktopSupported()) {
+                // java with desktop support
+                Desktop desktop = Desktop.getDesktop();
+                desktop.browse(new URI(url));
+            } else {
+                // no java desktop support
+                // fall back into dark ages
+                String osName = System.getProperty("os.name");
 
-        var desktop = Desktop.getDesktop();
-
-        try{
-
-            desktop.browse(new URI("http://localhost:8080"));
-
-        }catch(Exception e){
-            e.printStackTrace();
-            throw e;
-
+                if (osName.toLowerCase().contains("linux")) {
+                    // probably linux/unix
+                    Runtime runtime = Runtime.getRuntime();
+                    runtime.exec("xdg-open " + url);
+                } else if (osName.toLowerCase().contains("windows")) {
+                    // older windows
+                    Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+                } else if (osName.toLowerCase().contains("mac")) {
+                    // probably mac os
+                    Class.forName("com.apple.eio.FileManager").getDeclaredMethod("openURL", String.class).invoke(null,
+                            url);
+                }
+            }
+        } catch (IOException | URISyntaxException | IllegalAccessException | IllegalArgumentException
+                 | InvocationTargetException | NoSuchMethodException | SecurityException | ClassNotFoundException e) {
+            log.error("Could not start browser!", e);
+            e.fillInStackTrace();
         }
-
     }
 
 }
